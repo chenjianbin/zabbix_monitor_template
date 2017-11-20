1619a7cccd1a0b10de17a27411830e15c1e77ded
chenjianbin <546391242@qq.com>
2017-11-20 15:17:00 +0800

    Update mysql_slave_discovery.py
    xx

--- mysqlslave/mysql_slave_discovery.py
+++ mysqlslave/mysql_slave_discovery.py
@@ -5,7 +5,7 @@ import re
 MYSQL_PATH = '/data0/mysql/'
 
 def discover(path):
-        ports = [ {'{#MYSQLPORT}':d.name} for d in Path(path).glob('*') if d.is_dir() and re.fullmatch('[1-5]?[0-9]{1,4}', d.name) ]
+        ports = [ {'{#MYSQLPORT}':d.name} for d in Path(path).glob('*') if d.is_dir() and re.fullmatch('[1-5]?[0-9]{1,4}', d.name) and d.name != '3306' ]
         print(json.dumps({'data':ports},sort_keys=True,indent=4,separators=(',',':')))
 
 if __name__ == '__main__':
